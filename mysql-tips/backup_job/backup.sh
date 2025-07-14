c63da0f0b8a5d6dff648f5c7455a4aa13ebff4a8
wisnuflash <57955630+wisnuflash@users.noreply.github.com>
2025-07-14 13:24:10 +0700

    Update backup.sh

--- mysql-tips/backup_job/backup.sh
+++ mysql-tips/backup_job/backup.sh
@@ -6,10 +6,9 @@ DATE=$(date +%F)
 #LOG_FILE="$BACKUP_DIR_BASE/$JOB_NAME_log_$DATE.txt"
 
 # === Daftar Job Backup ===
-# Format: "NAMA_JOB|HOST|PORT|USER|PASSWORD"
+# Format: "NAMA_JOB|HOST|PORT|USER|PASSWORD|FOLDER"
 JOBS=(
-  "dblive|localhost|3306|root|password|new-dblive"
-#  "panelcloud|192.168.23.25|3306|root|hfVyE75U9NcReAws|panelcloud"
+   "dbtest|localhost|3306|root|password|dbtest"
 )
 
 # === Fungsi Backup ===
@@ -22,6 +21,7 @@ backup_database() {
   FOLDER="$6"
 
   LOG_FILE="$BACKUP_DIR_BASE/${JOB_NAME}_log_$DATE.txt"
+  echo "dir backup: $LOG_FILE"
   BACKUP_DIR="$BACKUP_DIR_BASE/$FOLDER"
   mkdir -p "$BACKUP_DIR"
 
@@ -31,11 +31,11 @@ backup_database() {
   DBS=$(mysql -u "$USER" -p"$PASSWORD" -h"$HOST" -P"$PORT" -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql|sys)")
 
   for DB in $DBS; do
-    echo "Membackup database: $DB" | tee -a "$LOG_FILE"
+    echo "Membackup database: $DB"
     DUMP_FILE="$BACKUP_DIR/${DB}.sql"
     ZIP_FILE="$BACKUP_DIR/${DB}_${DATE}.zip"
 
-    mysqldump -u "$USER" -p"$PASSWORD" -h"$HOST" -P"$PORT" "$DB" > "$DUMP_FILE"
+    mysqldump -u "$USER" -p"$PASSWORD" -h"$HOST" -P"$PORT" "$DB" > "$DUMP_FILE" | tee -a "$LOG_FILE"
     
     if [ $? -eq 0 ]; then
       zip -j "$ZIP_FILE" "$DUMP_FILE"
@@ -53,6 +53,7 @@ backup_database() {
    -F "file=@$LOG_FILE" \
    >> "$LOG_FILE"
   echo "[$(date '+%F %T')] ✔️ Selesai backup untuk job: $JOB_NAME" | tee -a "$LOG_FILE"
+  echo "[$(date '+%F %T')] 📬 Notifikasi backup terkirim." | tee -a "$LOG_FILE"
 }
 
 # === Loop semua job ===
@@ -69,4 +70,4 @@ done
 #  -F "file=@$LOG_FILE" \
 #  >> "$LOG_FILE"
 
-echo "[$(date '+%F %T')] 📬 Notifikasi backup terkirim." | tee -a "$LOG_FILE"
+echo "[$(date '+%F %T')] ✅ Backuo Job Selesai"
